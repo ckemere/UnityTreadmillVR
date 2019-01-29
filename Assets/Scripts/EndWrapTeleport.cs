6bce8102e420acf4ae8c5d340ce5a70ac48b9f82
Caleb Kemere <ckemere@gmail.com>
2019-01-29 17:54:09 -0600

    Teleport and world-extension working!

--- Assets/Scripts/EndWrapTeleport.cs
+++ Assets/Scripts/EndWrapTeleport.cs
@@ -6,11 +6,27 @@ public class EndWrapTeleport : MonoBehaviour
 {
     public Transform teleportTarget;
     public GameObject player;
+    public bool IsFront;
+
+    private Vector3 playerInitialPos;
+    private float playerOffset;
+    void Start ()
+    {
+        playerInitialPos = player.transform.position; // Players never move in y or z
+        playerOffset = player.GetComponent<Renderer>().bounds.extents.x;
+    }
 
     void OnTriggerEnter(Collider Other)
     {
-        Debug.Log("Player entered trigger."); 
-        player.transform.position = teleportTarget.transform.position;
+        Debug.Log("Player entered trigger." + teleportTarget.name); 
+        if (IsFront)
+            player.transform.position =  
+                new Vector3(teleportTarget.transform.position.x - playerOffset,
+                    playerInitialPos.y, playerInitialPos.z);
+        else
+            player.transform.position =  
+                new Vector3(teleportTarget.transform.position.x + playerOffset,
+                    playerInitialPos.y, playerInitialPos.z);
     }
 
 }
---
+++ Assets/Scripts/SceneSetup.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSetup : MonoBehaviour
+{
+    public GameObject maze;
+    public GameObject ForwardTeleport;
+    public GameObject ForwardTeleportTarget;
+    public GameObject RearTeleport;
+    public GameObject RearTeleportTarget;
+
+    public int numberOfCopies;
+
+    public float hysteresis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Figure how big maze is. Go through this process in case it gets
+        // more complicated eventually.
+        Bounds bounds = new Bounds(maze.transform.position, Vector3.zero);
+ 
+        foreach(Renderer renderer in maze.GetComponentsInChildren<Renderer>())
+        {
+            bounds.Encapsulate(renderer.bounds);
+        }
+
+        // Clone maze ahead to give perception of an infinite environment
+        Vector3 currentOffset = new Vector3();
+        Quaternion rotation0 = new Quaternion();
+        for (int i = 0; i < numberOfCopies; i++)
+        {
+            currentOffset.x += bounds.size.x;
+            Object.Instantiate(maze, currentOffset, rotation0);
+        }
+
+        // Clone maze behind so that when we land off the end, that's set up
+        currentOffset.x = -bounds.size.x;
+        Object.Instantiate(maze, currentOffset, rotation0);
+
+
+        // Now, set up teleporters in the right positions:
+        // (1) One at the forward end of the maze
+        ForwardTeleport.transform.position = new Vector3(bounds.max.x,0.0f,0.0f);
+        ForwardTeleportTarget.transform.position = new Vector3(bounds.min.x, 0.0f, 0.0f);
+
+        // (1) One at the backward end of the maze
+        RearTeleport.transform.position = new Vector3(bounds.min.x - hysteresis,0.0f,0.0f);
+        RearTeleportTarget.transform.position = new Vector3(bounds.max.x  - hysteresis,0.0f,0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
