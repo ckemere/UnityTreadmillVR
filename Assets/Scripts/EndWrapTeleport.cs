76e9202bf79c58d9e2a7a815218161512692b6ff
Caleb Kemere <ckemere@gmail.com>
2019-01-29 08:48:07 -0600

    Initial Commit

---
+++ Assets/Scripts/EndWrapTeleport.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndWrapTeleport : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject player;
+
+    void OnTriggerEnter(Collider Other)
+    {
+        Debug.Log("Player entered trigger."); 
+        player.transform.position = teleportTarget.transform.position;
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    //private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Fixed Update is called once per physics tick
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveVertical, 0, 0); // x, y, z
+        
+        transform.position += movement*speed;
+        //rb.AddForce (movement*speed);
+    }
+}
